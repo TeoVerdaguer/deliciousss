2dac67360e9ae73ffda89a5d89d0d9e553565305
Mateo Verdaguer <mateoverdaguer123@gmail.com>
2022-05-11 01:05:12 -0300

    fix build errors

--- src/pages/Cuisine.jsx
+++ src/pages/Cuisine.jsx
@@ -1,4 +1,4 @@
-import React, { useState, UseEffect, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { motion } from "framer-motion";
 import { Link, useParams } from "react-router-dom";
